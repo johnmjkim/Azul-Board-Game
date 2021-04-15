503ba85bf09d9091fcd7928da04816718039a894
John <johnmjkim1216@gmail.com>
2021-04-15 13:49:59 +0900

    Group Assignment 2 Made BagTyped Interface
    Signed-off-by: John <johnmjkim1216@gmail.com>

---
+++ src/comp1110/ass2/BagTypedBoard.java
@@ -0,0 +1,54 @@
+package comp1110.ass2;
+
+public class BagTypedBoard implements BagTyped{
+
+    @Override
+    public void countTilesNumber(String State) {
+
+    }
+
+    @Override
+    public int getTilesNumber(char color) {
+        return 0;
+    }
+
+    @Override
+    public int getTotalTilesNumber() {
+        return 0;
+    }
+
+    @Override
+    public void removeTile(char color) {
+
+    }
+
+    @Override
+    public void removeAllTiles() {
+
+    }
+
+    @Override
+    public void addTile(char color) {
+
+    }
+
+    @Override
+    public void refillTiles(String refill) {
+
+    }
+
+    @Override
+    public boolean isStateEmpty() {
+        return false;
+    }
+
+    @Override
+    public String getStateString() {
+        return null;
+    }
+
+    @Override
+    public void updateState() {
+
+    }
+}
