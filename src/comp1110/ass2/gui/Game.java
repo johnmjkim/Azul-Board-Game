c5a66c1ef91e05eb469b43a61eade1c07f8e3907
John <johnmjkim1216@gmail.com>
2021-05-01 15:25:21 +0900

    Group Assignment 2 Game.java with useful GUI code
    Signed-off-by: John <johnmjkim1216@gmail.com>

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -3,8 +3,11 @@ package comp1110.ass2.gui;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Game extends Application {
     /* board layout */
@@ -90,9 +93,185 @@ public class Game extends Application {
     public class GameBoard extends Polygon{
 
     }
-    // Draggable Tiles Codes
-    /*
-
-     */
 
 }
+
+// Draggable Tiles Codes
+/*
+package comp1110.lab5;
+
+        import comp1110.lab2.Triangle;
+        import javafx.animation.Timeline;
+        import javafx.application.Application;
+        import javafx.scene.Group;
+        import javafx.scene.Scene;
+        import javafx.scene.paint.Color;
+        import javafx.scene.shape.Polygon;
+        import javafx.stage.Stage;
+
+        import java.util.ArrayList;
+
+// 1. Create a new JavaFX class
+public class Board extends Application {
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 519;
+    Stage window;
+    Group root;
+    Scene scene;
+    ArrayList<Triangle> Triangles = new ArrayList<Triangle>();
+    Triangle highlighted = null;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primarystage) throws Exception{
+        window = primarystage;
+        root = new Group();
+        // Draws 600 x 519 pixel window
+        scene = new Scene(root, WIDTH, HEIGHT);
+        window.setScene(scene);
+
+        // Set window title to "Board"
+        window.setTitle("Board");
+
+        // 2. Draw a triangle : Set polygon
+
+        Polygon p = new Polygon();
+        p.getPoints().addAll(new Double[]{0.0, -86.6, 100.0, 86.6, -100.0, 86.6});
+        p.setFill(Color.LIGHTGREY);
+        root.getChildren().add(p);
+
+
+
+        // 4. Fill the board with triangles & 5. Create a white border
+        //ArrayList<Triangle> Triangles = new ArrayList<Triangle>();
+        for(int i=-2; i <=2 ; i++){
+            for(int j=-1; j <=1 ; j++){
+                //System.out.println("(" + 100 * i + ", " + 173.2 * j + ")");
+                Triangles.add(new Triangle(100 * i, 173.2 * j, 196));
+            }
+        }
+        boolean isRotate = false;
+        int count = 0;
+        for(Triangle ts : Triangles){
+            if(count % 3 == 0){
+                if(count % 6 ==0){
+                    isRotate = false;
+                }
+                else{
+                    isRotate = true;
+                }
+            }
+            if(isRotate){
+                ts.setRotate(180);
+                Triangles.set(count, ts);
+            }
+            root.getChildren().add(ts);
+            count++;
+        }
+        // Extension 4. Create a draggable triangle
+        DraggableTriangle dt = new DraggableTriangle(300, 260, 200, this);
+        root.getChildren().add(dt);
+        // Show all
+        window.show();
+    }
+
+    // 7. Find the nearest triangle
+    public Triangle findNearestTriangle(double x, double y){
+        double nearest_dist = this.Triangles.get(0).distance(x, y);
+        for( Triangle t : this.Triangles){
+            if( t.distance(x, y) <= nearest_dist){
+                nearest_dist = t.distance(x, y);
+                this.highlighted = t;
+            }
+        }
+
+        //System.out.println(index + ", " + nearest_dist);
+        return this.highlighted;
+    }
+
+    // 8. Highlight the nearest
+    public void highlightedNearestTriangle(double x, double y){
+        int i=0;
+        this.highlighted = findNearestTriangle(x, y);
+        ArrayList<Triangle> ts = this.Triangles;
+        for( Triangle t : ts){
+            if( t.equals(this.highlighted) ){
+                //System.out.println(i + ", - > GREEN " + ", Dist " + t.distance(x, y) + ", at (" + x + "," +y +") to (" + t.getLayoutX() + ", " + t.getLayoutY() +")");
+                t.setFill(Color.GREEN);
+                this.Triangles.set(i, t);
+            }
+            else if( t.getFill().equals(Color.GREEN) ){
+                //System.out.println(i + ", - > LIGHTGREY " + ", Dist " + t.distance(x, y) + ", at (" + x + "," +y +") to (" + t.getLayoutX() + ", " + t.getLayoutY() +")");
+                t.setFill(Color.LIGHTGREY);
+                this.Triangles.set(i, t);
+            }
+            else{
+                //System.out.println(i + ", LIGHTGREY " + ", Dist " + t.distance(x, y) + ", at (" + x + "," +y +") to (" + t.getLayoutX() + ", " + t.getLayoutY() +")");
+            }
+            i++;
+        }
+    }
+
+    // 3. Create an inner class
+    public class Triangle extends Polygon {
+        double x, y, side;
+
+        public Triangle(double x, double y, double side){
+            this.x = x;
+            this.y = y;
+            this.side = side;
+            double half_length = side/2;
+            double apex_point = side * 86.6/200;
+            getPoints().addAll(new Double[]{0.0, -apex_point, half_length, apex_point, -half_length, apex_point});
+            setFill(Color.LIGHTGREY);
+            setLayoutX(x + WIDTH/2);
+            setLayoutY(y + HEIGHT/2);
+            //System.out.println("x (" + x + ") + WIDTH/2 (" + WIDTH/2 +") =" + (x + WIDTH/2) + ", y (" + y + ") + HEIGHT/2 (" + HEIGHT/2 + ") =" + (y + HEIGHT/2));
+            //System.out.println(": getLayoutX : " + getLayoutX() + ", getLayoutY :" + getLayoutY());
+        }
+        // 6. Add a distance calculator
+        private double distance(double x, double y){
+            return Math.sqrt(Math.pow(getLayoutX() - x, 2) + Math.pow(getLayoutY() - y, 2));
+        }
+    }
+
+    // Extension 1. Create an inner class
+    public class DraggableTriangle extends Triangle {
+        private Board board;
+        private double mousex, mousey;
+        private double posit_x, posit_y;
+        // Extension 2 : Add a constructor
+        public DraggableTriangle(double x, double y, double size, Board board){
+            super( x, y, size);
+            setFill(Color.RED);
+            // Extension 3 : Add a new field
+            this.board = board;
+            setLayoutX(super.getLayoutX());
+            setLayoutY(super.getLayoutY());
+            // Extension 5: Make the inner class draggable
+            this.toFront();
+            this.setOnMousePressed(event ->{
+                this.mousex = event.getSceneX();
+                this.mousey = event.getSceneY();
+                this.posit_x = this.getLayoutX();
+                this.posit_y = this.getLayoutY();
+            });
+            this.setOnMouseDragged(event ->{
+                setLayoutX(event.getSceneX() - mousex + this.posit_x);
+                setLayoutY(event.getSceneY() - mousey + this.posit_y);
+                board.highlightedNearestTriangle(getLayoutX(), getLayoutY());
+            });
+            // Extension 9 : Snap to nearest triangle
+            this.setOnMouseReleased(event ->{
+                setRotate(board.highlighted.getRotate());
+                setLayoutX(board.highlighted.getLayoutX());
+                setLayoutY(board.highlighted.getLayoutY());
+            });
+        }
+    }
+}
+
+ */
